035e25d0289b0c2686c882d7154f480e8c00dccd
Robin Bate Boerop <robin@bateboerop.name>
2011-03-19 23:31:59 +0000

    Changed license to Apache License, Version 2.0; Changed version to 3000.2.0.2; BSD license remains in effect

--- Text/XHtml/BlockTable.hs
+++ Text/XHtml/BlockTable.hs
@@ -1,5 +1,17 @@
 -- Copyright 1999-2011 Andy Gill and the Oregon Graduate Institute of Science
 --                     and Technology, and Bjorn Bringert
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License"); you may not
+-- use this file except in compliance with the License.  You may obtain a copy
+-- of the License at
+-- 
+--     http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+-- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+-- License for the specific language governing permissions and limitations under
+-- the License.
 --
 -- Contributors:
 --   Ian Lynagh <igloo@earth.li>
@@ -11,7 +23,7 @@
 -- Module      :  Text.XHtml.BlockTable
 -- Copyright   :  (c) Andy Gill, and the Oregon Graduate Institute of 
 --                    Science and Technology, 1999-2001
--- License     :  BSD-style (see the file LICENSE)
+-- License     :  Apache License, Version 2.0 (see the file LICENSE)
 -- Maintainer  :  Bjorn Bringert <bjorn@bringert.net>
 -- Stability   :  experimental
 -- Portability :  portable
--- Text/XHtml/Internals.hs
+++ Text/XHtml/Internals.hs
@@ -1,4 +1,16 @@
--- Copyright 1999-2011 The Text.XHtml Authors.
+-- Copyright 2011 The Text.XHtml Authors.
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License"); you may not
+-- use this file except in compliance with the License.  You may obtain a copy
+-- of the License at
+-- 
+--     http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+-- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+-- License for the specific language governing permissions and limitations under
+-- the License.
 --  
 -- Contributors:
 --   Ian Lynagh <igloo@earth.li>
@@ -11,7 +23,7 @@
 -- Copyright   :  (c) Andy Gill, and the Oregon Graduate Institute of 
 --                Science and Technology, 1999-2001,
 --                (c) Bjorn Bringert, 2004-2006
--- License     :  BSD-style (see the file LICENSE)
+-- License     :  Apache License, Version 2.0 (see the file LICENSE)
 -- Maintainer  :  Bjorn Bringert <bjorn@bringert.net>
 -- Stability   :  experimental
 -- Portability :  portable
